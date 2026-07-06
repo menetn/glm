1aa1aa686b97188e2ce3336f16ca9b8d30d97b6b
menetn <nicolasandrinmenet@gmail.com>
2026-07-06 17:36:56 +0200

    Add train_owt_smflm_lin_bias.sh script

---
+++ scripts/train_owt_smflm_lin_bias.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+DATA_DIR="/proj/moc-checkpoints/men/glm/datasets"
+
+CHECKPOINT_DIR="/proj/moc-checkpoints/men/glm/checkpoints/smflm_lin_bias"
+
+if [ "$DATA_DIR" = "YOUR_DATA_DIR" ] || [ "$CHECKPOINT_DIR" = "YOUR_CHECKPOINT_DIR" ]; then
+    echo "Error: DATA_DIR and CHECKPOINT_DIR must be set"
+    exit 1
+fi
+
+python -u -m main \
+  checkpointing.save_dir=$CHECKPOINT_DIR \
+  checkpointing.resume_from_ckpt=True \
+  loader.global_batch_size=512 \
+  loader.batch_size=32 \
+  loader.eval_batch_size=32 \
+  data=openwebtext-split \
+  data.cache_dir=$DATA_DIR \
+  wandb.project=owt_full \
+  wandb.name=owt_smflm_lin_bias \
+  model=small \
+  algo=smflm \
+  model.length=1024 \
+  sampling.num_sample_batches=1 \
+  sampling.solver=euler \
+  sampling.steps=[1024] \
+  trainer.max_steps=1000000 \
+  trainer.precision=bf16 \
+  optim.lr=3e-4 \
+  trainer.val_check_interval=5000 \
+  algo.double_temb=False \
+  callbacks.checkpoint_every_n_steps.every_n_train_steps=20000 \
